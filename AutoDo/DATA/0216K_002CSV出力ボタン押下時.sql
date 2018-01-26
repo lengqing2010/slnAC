7ede1c00da2889fcd875d69167f647071223c9ff
biglixil <liangjiu.xu@lixil.com>
2018-01-26 16:54:17 +0800

    com1

--- AutoDo/DATA/0216K_002CSV出力ボタン押下時.sql
+++ AutoDo/DATA/0216K_002CSV出力ボタン押下時.sql
@@ -1,17 +1,22 @@
+
 IF EXISTS(SELECT * 
           FROM   tempdb..sysobjects 
-          WHERE  id = Object_id('tempdb..#tmp0216k')) 
+          WHERE  id = Object_id('tempdb..#tmp0216k0955108118')) 
   BEGIN 
-      DROP TABLE #tmp0216k 
+      DROP TABLE #tmp0216k0955108118 
   END 
 
-SELECT tsj.tenpo_cd 
+SELECT  tsj.tenpo_cd 
        ,tsj.nouhin_yotei_date 
        ,tsj.truck_no 
        ,tsj.tc_center_cd 
        ,mj.tenpo_mei_kanji 
        ,tsj.torihikisaki_cd 
-       ,tsj.konpou_no 
+        ,case when data_sakusei_kbn = '01' then 
+                substring(tsj.konpou_no, 12,5) + '-' + substring(tsj.konpou_no,18,4)  
+        else 
+                substring(tsj.konpou_no,1,4) + '-' + substring(tsj.konpou_no,5,2) + '-' + substring(tsj.konpou_no,7,4)
+        end konpou_no
        ,tsj.denpyou_no 
        ,tsj.gyou_no 
        ,tsj.syouhin_cd_jisya 
@@ -22,17 +27,19 @@ SELECT tsj.tenpo_cd
           WHEN LEFT(tsj.syouhin_cd_jisya, 1) = '9' THEN msck.syouhin_cd_jisya 
           ELSE tsj.syouhin_cd_jisya 
         END AS syouhin_cd_jisya_key 
-INTO   #tmp0216k 
+       ,tsj.data_sakusei_kbn 
+       ,tsj.juuki_no 
+INTO   #tmp0216k0955108118 
 FROM   t_syukka_jisseki tsj 
        INNER JOIN m_jigyousyo mj 
                ON mj.tenpo_cd = tsj.tc_center_cd 
        LEFT JOIN m_syouhin_cd_kanri msck 
               ON msck.syouhin_cd_tokubai = tsj.syouhin_cd_jisya 
                  AND msck.daihyou_jan_kbn = '1' 
-WHERE  tsj.truck_no = '201711140653681' 
-
+WHERE  tsj.truck_no in('201711100653685','201711140653681')  
+   AND tsj.tenpo_cd = '1030'
 SELECT main.tenpo_cd 
-       ,main.nouhin_yotei_date 
+       ,CONVERT(char(10), CONVERT(DateTime,main.nouhin_yotei_date), 111) as nouhin_yotei_date 
        ,main.truck_no 
        ,main.tc_center_cd 
        ,main.tenpo_mei_kanji 
@@ -40,7 +47,7 @@ SELECT main.tenpo_cd
        ,main.konpou_no 
        ,main.denpyou_no 
        ,main.gyou_no 
-       ,main.syouhin_cd_jisya 
+       ,main.syouhin_cd_jisya_key as syouhin_cd_jisya 
        ,main.syouhin_cd_jan_1 
        ,msj.hyoujiyou_syouhin_mei_kanji 
        ,main.hattyuu_suu 
@@ -48,7 +55,9 @@ SELECT main.tenpo_cd
        ,thj.zaiko 
        ,mud.uriba_address 
        ,mmbj.hattyuu_check_kbn 
-FROM   #tmp0216k main 
+       ,main.data_sakusei_kbn 
+       ,main.juuki_no 
+FROM   #tmp0216k0955108118 main 
        INNER JOIN m_syouhin_jyouhou msj 
                ON msj.syouhin_cd_jisya = main.syouhin_cd_jisya_key 
        INNER JOIN t_hibetu_jisseki thj 
@@ -56,22 +65,10 @@ FROM   #tmp0216k main
                   AND thj.syouhin_cd_jisya = main.syouhin_cd_jisya_key 
                   AND thj.sakusei_date = main.nouhin_yotei_date 
        --(この条件） 
-       INNER JOIN m_mise_betu_jyouhou mmbj 
+       LEFT JOIN m_mise_betu_jyouhou mmbj 
                ON mmbj.tenpo_cd = main.tenpo_cd 
                   AND mmbj.syouhin_cd_jisya = main.syouhin_cd_jisya_key 
        INNER JOIN m_uriba_daittyou mud 
                ON mud.tenpo_cd = main.tenpo_cd 
                   AND mud.syouhin_cd_jisya = main.syouhin_cd_jisya_key 
-ORDER BY main.truck_no
-
-
-
-
-
-
-
-
-
-
-
-
+ORDER BY main.truck_no,main.nouhin_yotei_date
--- AutoDo/DATA/999帳票パス.sql
+++ AutoDo/DATA/999帳票パス.sql
@@ -4,3 +4,7 @@ E:\CIFS01\honbanikou_01\iWFM-t\forms\yuusi\test
 
 
 E:\CIFS01\honbanikou_01\iWFM-t\forms\VIVA-SA
+
+\\192.168.118.220\iWFM-t$\data\VIVA-SA
+
+\\192.168.118.220\iWFM-t$\data\VIVA-SA
