863f6e34e1ac4cdf325a8ab3406f9e0812abacc7
lengqing2010 <lengqing2010@163.com>
2018-07-11 20:41:39 +0800

    ss

---
+++ AutoWeb/Form1.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.rtbxMsg = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'rtbxMsg
+        '
+        Me.rtbxMsg.Location = New System.Drawing.Point(77, 250)
+        Me.rtbxMsg.Name = "rtbxMsg"
+        Me.rtbxMsg.Size = New System.Drawing.Size(820, 199)
+        Me.rtbxMsg.TabIndex = 0
+        Me.rtbxMsg.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1093, 599)
+        Me.Controls.Add(Me.rtbxMsg)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents rtbxMsg As System.Windows.Forms.RichTextBox
+
+End Class
