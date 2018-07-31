a7fc0d299f870a6c8be0ecc210eb273b6ded058b
lengqing2010 <lengqing2010@163.com>
2018-07-31 16:50:30 +0800

    aaa
    aa

---
+++ IME/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace IME
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(85, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(85, 146);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 278);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ IME/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+
+namespace IME
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("Imm32.dll")]
+        public static extern IntPtr ImmGetContext(IntPtr hWnd);
+
+        [DllImport("Imm32.dll")]
+        private static extern bool ImmReleaseContext(IntPtr hWnd, IntPtr hIMC);
+
+        [DllImport("Imm32.dll")]
+        private static extern bool ImmGetConversionStatus(IntPtr hIMC, ref int fdwConversion, ref int fdwSentence);
+
+        [DllImport("Imm32.dll")]
+        private static extern bool ImmGetOpenStatus(IntPtr hIMC);
+
+        [DllImport("Imm32.dll")]
+        private static extern bool ImmSetOpenStatus(IntPtr hIMC, long fOpen);
+
+        [DllImport("Imm32.dll")]
+        private static extern bool ImmSetConversionStatus(IntPtr hIMC, int fdwConversion, int fdwSentence);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // 入力モードを示す定数
+        /// <summary>英数字入力モード</summary>
+        private const int IME_CMODE_ALPHANUMERIC = 0x0;
+        /// <summary>言語依存入力モード</summary>
+        private const int IME_CMODE_NATIVE = 0x1;
+        /// <summary>日本語入力モード</summary>
+        private const int IME_CMODE_JAPANESE = IME_CMODE_NATIVE;
+        /// <summary>カタカナ入力モード</summary>
+        private const int IME_CMODE_KATAKANA = 0x2;
+        /// <summary>言語入力モード</summary>
+        private const int IME_CMODE_LANGUAGE = 0x3;
+        /// <summary>全角入力モード</summary>
+        private const int IME_CMODE_FULLSHAPE = 0x8;
+        /// <summary>ローマ字入力モード</summary>
+        private const int IME_CMODE_ROMAN = 0x10;
+
+        // 変換モードを示す定数の宣言
+        /// <summary>無変換モード</summary>
+        private const int IME_SMODE_NONE = 0x0;
+        /// <summary>複数文字変換モード</summary>
+        private const int IME_SMODE_PLAURALCLAUSE = 0x1;
+        /// <summary>単一文字変換モード</summary>
+        private const int IME_SMODE_SINGLECONVERT = 0x2;
+        /// <summary>自動変換モード</summary>
+        private const int IME_SMODE_AUTOMATIC = 0x4;
+        /// <summary>予測変換モード</summary>
+        private const int IME_SMODE_PHRASEPREDICT = 0x8;
+
+
+        /// <summary>
+        /// IMEモードを切り替える
+        /// </summary>
+        /// <param name="handle">ウィンドウのハンドル</param>
+        /// <param name="imeMode">IMEモード</param>
+        private static void ChangeImeMode(IntPtr handle, ImeMode imeMode)
+        {
+            IntPtr lngInputContextHandle;
+            int lngStatusIMEConversion = 0;
+            bool lngWin32apiResultCode;
+
+            // ウィンドウに関連付けされた入力コンテキストを取得
+            lngInputContextHandle = ImmGetContext(handle);
+
+            if (lngInputContextHandle != new IntPtr(0))
+            {
+                // IMEをオープン
+                lngWin32apiResultCode = ImmSetOpenStatus(lngInputContextHandle, 1);
+
+                // 初期入力モードを指定
+                switch (imeMode)
+                {
+                    case ImeMode.AlphaFull: // 全角英数字
+                        lngStatusIMEConversion = IME_CMODE_FULLSHAPE;
+                        break;
+                    case ImeMode.Hiragana: // 全角ひらがな
+                        lngStatusIMEConversion = IME_CMODE_NATIVE | IME_CMODE_FULLSHAPE;
+                        break;
+                    case ImeMode.Katakana: // 全角カナ
+                        lngStatusIMEConversion = IME_CMODE_NATIVE | IME_CMODE_FULLSHAPE | IME_CMODE_KATAKANA;
+                        break;
+                    case ImeMode.KatakanaHalf: // 半角カナ
+                        lngStatusIMEConversion = IME_CMODE_NATIVE | IME_CMODE_KATAKANA;
+                        break;
+                    default:
+                        // 半角英数字、その他
+                        lngWin32apiResultCode = ImmSetOpenStatus(lngInputContextHandle, 0);
+                        lngWin32apiResultCode = ImmReleaseContext(handle, lngInputContextHandle);
+                        return;
+                }
+
+                // IMEの初期方式を設定
+                lngWin32apiResultCode = ImmSetConversionStatus(lngInputContextHandle, lngStatusIMEConversion, IME_SMODE_NONE);
+
+                // 入力コンテキストを開放
+                lngWin32apiResultCode = ImmReleaseContext(handle, lngInputContextHandle);
+            }
+        }
+
+        // ひらがなにしてみる
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            ChangeImeMode(this.Handle, ImeMode.Hiragana);
+        }
+
+        // IMEをOFFにする
+        private void textBox3_Enter(object sender, EventArgs e)
+        {
+            ChangeImeMode(this.Handle, ImeMode.NoControl);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            ChangeImeMode(this.Handle, ImeMode.Hiragana);
+        }
+
+    }
+}
