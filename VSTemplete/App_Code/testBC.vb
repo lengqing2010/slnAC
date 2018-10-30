a5a7ad4f25af5133165f3c3b9799bf31ead9ef3b
lengqing2010 <lengqing2010@163.com>
2018-10-30 09:19:49 +0800

    7

--- VSTemplete/App_Code/testBC.vb
+++ VSTemplete/App_Code/testBC.vb
@@ -10,113 +10,113 @@ Imports System.Configuration.ConfigurationSettings
 Imports System.Collections.Generic
 
 Public Class MEdpBC
-Public DA AS NEW MEdpDA
+    Public DA As New MEdpDA
 
-''' <summary>
-''' 
-''' 情報を検索する
-''' </summary>
-'''<param name="edpNo_key">edp_no</param>
-''' <returns>情報</returns>
-''' <remarks></remarks>
-''' <history>
-''' <para>2018/10/30  李松涛さん 新規作成 </para>
-''' </history>
+    ''' <summary>
+    ''' 
+    ''' 情報を検索する
+    ''' </summary>
+    '''<param name="edpNo_key">edp_no</param>
+    ''' <returns>情報</returns>
+    ''' <remarks></remarks>
+    ''' <history>
+    ''' <para>2018/10/30  李松涛さん 新規作成 </para>
+    ''' </history>
 
-Public Function SelMEdp(           Byval edpNo_key AS String) As Data.DataTable
-    'EMAB障害対応情報の格納処理
-    EMAB.AddMethodEntrance(MyClass.GetType.FullName & "." & MyMethod.GetCurrentMethod.Name , _
-           edpNo_key)
-    'SQLコメント
-    Return DA.SelMEdp( _
-           edpNo_key)
-End Function
+    Public Function SelMEdp(ByVal edpNo_key As String) As Data.DataTable
+        'EMAB障害対応情報の格納処理
+        EMAB.AddMethodEntrance(MyClass.GetType.FullName & "." & MyMethod.GetCurrentMethod.Name, _
+               edpNo_key)
+        'SQLコメント
+        Return DA.SelMEdp( _
+               edpNo_key)
+    End Function
 
-''' <summary>
-''' 
-''' 情報を更新する
-''' </summary>
-'''<param name="edpNo_key">edp_no</param>
-'''<param name="edpNo">edp_no</param>
-'''<param name="edpMei">edp_mei</param>
-'''<param name="edpExp">edp_exp</param>
-''' <returns>情報</returns>
-''' <remarks></remarks>
-''' <history>
-''' <para>2018/10/30  李松涛さん 新規作成 </para>
-''' </history>
+    ''' <summary>
+    ''' 
+    ''' 情報を更新する
+    ''' </summary>
+    '''<param name="edpNo_key">edp_no</param>
+    '''<param name="edpNo">edp_no</param>
+    '''<param name="edpMei">edp_mei</param>
+    '''<param name="edpExp">edp_exp</param>
+    ''' <returns>情報</returns>
+    ''' <remarks></remarks>
+    ''' <history>
+    ''' <para>2018/10/30  李松涛さん 新規作成 </para>
+    ''' </history>
 
-Public Function UpdMEdp(           Byval edpNo_key AS String, _
-           Byval edpNo AS String, _
-           Byval edpMei AS String, _
-           Byval edpExp AS String) As Boolean
-    'EMAB障害対応情報の格納処理
-    EMAB.AddMethodEntrance(MyClass.GetType.FullName & "." & MyMethod.GetCurrentMethod.Name , _
-           edpNo_key, _
-           edpNo, _
-           edpMei, _
-           edpExp)
-    'SQLコメント
-    '--**テーブル： : m_edp
-    Return DA.UpdMEdp( _
-           edpNo_key, _
-           edpNo, _
-           edpMei, _
-           edpExp)
+    Public Function UpdMEdp(ByVal edpNo_key As String, _
+               ByVal edpNo As String, _
+               ByVal edpMei As String, _
+               ByVal edpExp As String) As Boolean
+        'EMAB障害対応情報の格納処理
+        EMAB.AddMethodEntrance(MyClass.GetType.FullName & "." & MyMethod.GetCurrentMethod.Name, _
+               edpNo_key, _
+               edpNo, _
+               edpMei, _
+               edpExp)
+        'SQLコメント
+        '--**テーブル： : m_edp
+        Return DA.UpdMEdp( _
+               edpNo_key, _
+               edpNo, _
+               edpMei, _
+               edpExp)
 
-End Function
+    End Function
 
-''' <summary>
-''' 
-''' 情報を登録する
-''' </summary>
-'''<param name="edpNo">edp_no</param>
-'''<param name="edpMei">edp_mei</param>
-'''<param name="edpExp">edp_exp</param>
-''' <returns>情報</returns>
-''' <remarks></remarks>
-''' <history>
-''' <para>2018/10/30  李松涛さん 新規作成 </para>
-''' </history>
+    ''' <summary>
+    ''' 
+    ''' 情報を登録する
+    ''' </summary>
+    '''<param name="edpNo">edp_no</param>
+    '''<param name="edpMei">edp_mei</param>
+    '''<param name="edpExp">edp_exp</param>
+    ''' <returns>情報</returns>
+    ''' <remarks></remarks>
+    ''' <history>
+    ''' <para>2018/10/30  李松涛さん 新規作成 </para>
+    ''' </history>
 
-Public Function InsMEdp(           Byval edpNo AS String, _
-           Byval edpMei AS String, _
-           Byval edpExp AS String) As Boolean
-    'EMAB障害対応情報の格納処理
-    EMAB.AddMethodEntrance(MyClass.GetType.FullName & "." & MyMethod.GetCurrentMethod.Name , _
-           edpNo, _
-           edpMei, _
-           edpExp)
-    'SQLコメント
-    '--**テーブル： : m_edp
-    Return DA.InsMEdp( _
-           edpNo, _
-           edpMei, _
-           edpExp)
+    Public Function InsMEdp(ByVal edpNo As String, _
+               ByVal edpMei As String, _
+               ByVal edpExp As String) As Boolean
+        'EMAB障害対応情報の格納処理
+        EMAB.AddMethodEntrance(MyClass.GetType.FullName & "." & MyMethod.GetCurrentMethod.Name, _
+               edpNo, _
+               edpMei, _
+               edpExp)
+        'SQLコメント
+        '--**テーブル： : m_edp
+        Return DA.InsMEdp( _
+               edpNo, _
+               edpMei, _
+               edpExp)
 
-End Function
+    End Function
 
-''' <summary>
-''' 
-''' 情報を削除する
-''' </summary>
-'''<param name="edpNo_key">edp_no</param>
-''' <returns>情報</returns>
-''' <remarks></remarks>
-''' <history>
-''' <para>2018/10/30  李松涛さん 新規作成 </para>
-''' </history>
+    ''' <summary>
+    ''' 
+    ''' 情報を削除する
+    ''' </summary>
+    '''<param name="edpNo_key">edp_no</param>
+    ''' <returns>情報</returns>
+    ''' <remarks></remarks>
+    ''' <history>
+    ''' <para>2018/10/30  李松涛さん 新規作成 </para>
+    ''' </history>
 
-Public Function DelMEdp(           Byval edpNo_key AS String) As Boolean
-    'EMAB障害対応情報の格納処理
-    EMAB.AddMethodEntrance(MyClass.GetType.FullName & "." & MyMethod.GetCurrentMethod.Name , _
-           edpNo_key)
-    'SQLコメント
-    '--**テーブル： : m_edp
-    Return DA.DelMEdp( _
-           edpNo_key)
+    Public Function DelMEdp(ByVal edpNo_key As String) As Boolean
+        'EMAB障害対応情報の格納処理
+        EMAB.AddMethodEntrance(MyClass.GetType.FullName & "." & MyMethod.GetCurrentMethod.Name, _
+               edpNo_key)
+        'SQLコメント
+        '--**テーブル： : m_edp
+        Return DA.DelMEdp( _
+               edpNo_key)
 
 
-End Function
+    End Function
 
 End Class
--- VSTemplete/App_Code/testDA.vb
+++ VSTemplete/App_Code/testDA.vb
@@ -11,187 +11,187 @@ Imports System.Collections.Generic
 
 Public Class MEdpDA
 
-''' <summary>
-''' 
-''' 情報を検索する
-''' </summary>
-'''<param name="edpNo_key">edp_no</param>
-''' <returns>情報</returns>
-''' <remarks></remarks>
-''' <history>
-''' <para>2018/10/30  李松涛さん 新規作成 </para>
-''' </history>
+    ''' <summary>
+    ''' 
+    ''' 情報を検索する
+    ''' </summary>
+    '''<param name="edpNo_key">edp_no</param>
+    ''' <returns>情報</returns>
+    ''' <remarks></remarks>
+    ''' <history>
+    ''' <para>2018/10/30  李松涛さん 新規作成 </para>
+    ''' </history>
 
-Public Function SelMEdp(           Byval edpNo_key AS String) As Data.DataTable
-    'EMAB障害対応情報の格納処理
-    EMAB.AddMethodEntrance(MyClass.GetType.FullName & "." & MyMethod.GetCurrentMethod.Name , _
-           edpNo_key)
-    'SQLコメント
-    '--**テーブル： : m_edp
-    Dim sb As New StringBuilder
-'SQL文
-    sb.AppendLine("SELECT")
-    sb.AppendLine("edp_no")   'edp_no
-    sb.AppendLine(", edp_mei")   'edp_mei
-    sb.AppendLine(", edp_exp")   'edp_exp
+    Public Function SelMEdp(ByVal edpNo_key As String) As Data.DataTable
+        'EMAB障害対応情報の格納処理
+        EMAB.AddMethodEntrance(MyClass.GetType.FullName & "." & MyMethod.GetCurrentMethod.Name, _
+               edpNo_key)
+        'SQLコメント
+        '--**テーブル： : m_edp
+        Dim sb As New StringBuilder
+        'SQL文
+        sb.AppendLine("SELECT")
+        sb.AppendLine("edp_no")   'edp_no
+        sb.AppendLine(", edp_mei")   'edp_mei
+        sb.AppendLine(", edp_exp")   'edp_exp
 
-    sb.AppendLine("FROM m_edp")
-    sb.AppendLine("WHERE 1=1")
-    If edpNo_key<>"" Then
-    sb.AppendLine("AND edp_no=@edp_no_key")   'edp_no
-End If
+        sb.AppendLine("FROM m_edp")
+        sb.AppendLine("WHERE 1=1")
+        If edpNo_key <> "" Then
+            sb.AppendLine("AND edp_no=@edp_no_key")   'edp_no
+        End If
 
-    'バラメタ格納
-    Dim paramList As New List(Of SqlParameter)
-    paramList.Add(MakeParam("@edp_no_key", SqlDbType.VarChar, 20, edpNo_key))
+        'バラメタ格納
+        Dim paramList As New List(Of SqlParameter)
+        paramList.Add(MakeParam("@edp_no_key", SqlDbType.VarChar, 20, edpNo_key))
 
-    Dim dsInfo As New Data.DataSet
-    FillDataset(DataAccessManager.Connection, CommandType.Text, sb.ToString(), dsInfo, "m_edp", paramList.ToArray)
+        Dim dsInfo As New Data.DataSet
+        FillDataset(DataAccessManager.Connection, CommandType.Text, sb.ToString(), dsInfo, "m_edp", paramList.ToArray)
 
-    Return dsInfo.Tables("m_edp")
+        Return dsInfo.Tables("m_edp")
 
-End Function
+    End Function
 
-''' <summary>
-''' 
-''' 情報を更新する
-''' </summary>
-'''<param name="edpNo_key">edp_no</param>
-'''<param name="edpNo">edp_no</param>
-'''<param name="edpMei">edp_mei</param>
-'''<param name="edpExp">edp_exp</param>
-''' <returns>情報</returns>
-''' <remarks></remarks>
-''' <history>
-''' <para>2018/10/30  李松涛さん 新規作成 </para>
-''' </history>
+    ''' <summary>
+    ''' 
+    ''' 情報を更新する
+    ''' </summary>
+    '''<param name="edpNo_key">edp_no</param>
+    '''<param name="edpNo">edp_no</param>
+    '''<param name="edpMei">edp_mei</param>
+    '''<param name="edpExp">edp_exp</param>
+    ''' <returns>情報</returns>
+    ''' <remarks></remarks>
+    ''' <history>
+    ''' <para>2018/10/30  李松涛さん 新規作成 </para>
+    ''' </history>
 
-Public Function UpdMEdp(           Byval edpNo_key AS String, _
-           Byval edpNo AS String, _
-           Byval edpMei AS String, _
-           Byval edpExp AS String) As Boolean
-    'EMAB障害対応情報の格納処理
-    EMAB.AddMethodEntrance(MyClass.GetType.FullName & "." & MyMethod.GetCurrentMethod.Name , _
-           edpNo_key, _
-           edpNo, _
-           edpMei, _
-           edpExp)
-    'SQLコメント
-    '--**テーブル： : m_edp
-    Dim sb As New StringBuilder
-'SQL文
-    sb.AppendLine("UPDATE m_edp")
-    sb.AppendLine("SET")
-    sb.AppendLine("edp_no=@edp_no")   'edp_no
-    sb.AppendLine(", edp_mei=@edp_mei")   'edp_mei
-    sb.AppendLine(", edp_exp=@edp_exp")   'edp_exp
+    Public Function UpdMEdp(ByVal edpNo_key As String, _
+               ByVal edpNo As String, _
+               ByVal edpMei As String, _
+               ByVal edpExp As String) As Boolean
+        'EMAB障害対応情報の格納処理
+        EMAB.AddMethodEntrance(MyClass.GetType.FullName & "." & MyMethod.GetCurrentMethod.Name, _
+               edpNo_key, _
+               edpNo, _
+               edpMei, _
+               edpExp)
+        'SQLコメント
+        '--**テーブル： : m_edp
+        Dim sb As New StringBuilder
+        'SQL文
+        sb.AppendLine("UPDATE m_edp")
+        sb.AppendLine("SET")
+        sb.AppendLine("edp_no=@edp_no")   'edp_no
+        sb.AppendLine(", edp_mei=@edp_mei")   'edp_mei
+        sb.AppendLine(", edp_exp=@edp_exp")   'edp_exp
 
-    sb.AppendLine("FROM m_edp")
-    sb.AppendLine("WHERE 1=1")
-    If edpNo_key<>"" Then
-    sb.AppendLine("AND edp_no=@edp_no_key")   'edp_no
-End If
+        sb.AppendLine("FROM m_edp")
+        sb.AppendLine("WHERE 1=1")
+        If edpNo_key <> "" Then
+            sb.AppendLine("AND edp_no=@edp_no_key")   'edp_no
+        End If
 
-    'バラメタ格納
-    Dim paramList As New List(Of SqlParameter)
-    paramList.Add(MakeParam("@edp_no_key", SqlDbType.VarChar, 20, edpNo_key))
+        'バラメタ格納
+        Dim paramList As New List(Of SqlParameter)
+        paramList.Add(MakeParam("@edp_no_key", SqlDbType.VarChar, 20, edpNo_key))
 
-    paramList.Add(MakeParam("@edp_no", SqlDbType.VarChar, 20, edpNo))
-    paramList.Add(MakeParam("@edp_mei", SqlDbType.VarChar, 200, edpMei))
-    paramList.Add(MakeParam("@edp_exp", SqlDbType.VarChar, 1000, edpExp))
-
-
-    SQLHelper.ExecuteNonQuery(DataAccessManager.Connection, CommandType.Text, sb.ToString(), paramList.ToArray) 
-
-    Return True 
-
-End Function
-
-''' <summary>
-''' 
-''' 情報を登録する
-''' </summary>
-'''<param name="edpNo">edp_no</param>
-'''<param name="edpMei">edp_mei</param>
-'''<param name="edpExp">edp_exp</param>
-''' <returns>情報</returns>
-''' <remarks></remarks>
-''' <history>
-''' <para>2018/10/30  李松涛さん 新規作成 </para>
-''' </history>
-
-Public Function InsMEdp(           Byval edpNo AS String, _
-           Byval edpMei AS String, _
-           Byval edpExp AS String) As Boolean
-    'EMAB障害対応情報の格納処理
-    EMAB.AddMethodEntrance(MyClass.GetType.FullName & "." & MyMethod.GetCurrentMethod.Name , _
-           edpNo, _
-           edpMei, _
-           edpExp)
-    'SQLコメント
-    '--**テーブル： : m_edp
-    Dim sb As New StringBuilder
-'SQL文
-    sb.AppendLine("INSERT INTO  m_edp")
-    sb.AppendLine("(")
-    sb.AppendLine("edp_no")   'edp_no
-    sb.AppendLine(", edp_mei")   'edp_mei
-    sb.AppendLine(", edp_exp")   'edp_exp
-
-    sb.AppendLine(")")
-    sb.AppendLine("VALUES(")
-    sb.AppendLine("@edp_no")   'edp_no
-    sb.AppendLine(", @edp_mei")   'edp_mei
-    sb.AppendLine(", @edp_exp")   'edp_exp
-
-    sb.AppendLine(")")
-    'バラメタ格納
-    Dim paramList As New List(Of SqlParameter)
-    paramList.Add(MakeParam("@edp_no", SqlDbType.VarChar, 20, edpNo))
-    paramList.Add(MakeParam("@edp_mei", SqlDbType.VarChar, 200, edpMei))
-    paramList.Add(MakeParam("@edp_exp", SqlDbType.VarChar, 1000, edpExp))
+        paramList.Add(MakeParam("@edp_no", SqlDbType.VarChar, 20, edpNo))
+        paramList.Add(MakeParam("@edp_mei", SqlDbType.VarChar, 200, edpMei))
+        paramList.Add(MakeParam("@edp_exp", SqlDbType.VarChar, 1000, edpExp))
 
 
-    SQLHelper.ExecuteNonQuery(DataAccessManager.Connection, CommandType.Text, sb.ToString(), paramList.ToArray) 
+        SQLHelper.ExecuteNonQuery(DataAccessManager.Connection, CommandType.Text, sb.ToString(), paramList.ToArray)
 
-    Return True 
+        Return True
 
-End Function
+    End Function
 
-''' <summary>
-''' 
-''' 情報を削除する
-''' </summary>
-'''<param name="edpNo_key">edp_no</param>
-''' <returns>情報</returns>
-''' <remarks></remarks>
-''' <history>
-''' <para>2018/10/30  李松涛さん 新規作成 </para>
-''' </history>
+    ''' <summary>
+    ''' 
+    ''' 情報を登録する
+    ''' </summary>
+    '''<param name="edpNo">edp_no</param>
+    '''<param name="edpMei">edp_mei</param>
+    '''<param name="edpExp">edp_exp</param>
+    ''' <returns>情報</returns>
+    ''' <remarks></remarks>
+    ''' <history>
+    ''' <para>2018/10/30  李松涛さん 新規作成 </para>
+    ''' </history>
 
-Public Function DelMEdp(           Byval edpNo_key AS String) As Boolean
-    'EMAB障害対応情報の格納処理
-    EMAB.AddMethodEntrance(MyClass.GetType.FullName & "." & MyMethod.GetCurrentMethod.Name , _
-           edpNo_key)
-    'SQLコメント
-    '--**テーブル： : m_edp
-    Dim sb As New StringBuilder
-'SQL文
-    sb.AppendLine("DELETE FROM m_edp")
-    sb.AppendLine("WHERE 1=1")
-    If edpNo_key<>"" Then
-    sb.AppendLine("AND edp_no=@edp_no_key")   'edp_no
-End If
+    Public Function InsMEdp(ByVal edpNo As String, _
+               ByVal edpMei As String, _
+               ByVal edpExp As String) As Boolean
+        'EMAB障害対応情報の格納処理
+        EMAB.AddMethodEntrance(MyClass.GetType.FullName & "." & MyMethod.GetCurrentMethod.Name, _
+               edpNo, _
+               edpMei, _
+               edpExp)
+        'SQLコメント
+        '--**テーブル： : m_edp
+        Dim sb As New StringBuilder
+        'SQL文
+        sb.AppendLine("INSERT INTO  m_edp")
+        sb.AppendLine("(")
+        sb.AppendLine("edp_no")   'edp_no
+        sb.AppendLine(", edp_mei")   'edp_mei
+        sb.AppendLine(", edp_exp")   'edp_exp
 
-    'バラメタ格納
-    Dim paramList As New List(Of SqlParameter)
-    paramList.Add(MakeParam("@edp_no_key", SqlDbType.VarChar, 20, edpNo_key))
+        sb.AppendLine(")")
+        sb.AppendLine("VALUES(")
+        sb.AppendLine("@edp_no")   'edp_no
+        sb.AppendLine(", @edp_mei")   'edp_mei
+        sb.AppendLine(", @edp_exp")   'edp_exp
+
+        sb.AppendLine(")")
+        'バラメタ格納
+        Dim paramList As New List(Of SqlParameter)
+        paramList.Add(MakeParam("@edp_no", SqlDbType.VarChar, 20, edpNo))
+        paramList.Add(MakeParam("@edp_mei", SqlDbType.VarChar, 200, edpMei))
+        paramList.Add(MakeParam("@edp_exp", SqlDbType.VarChar, 1000, edpExp))
 
 
-    SQLHelper.ExecuteNonQuery(DataAccessManager.Connection, CommandType.Text, sb.ToString(), paramList.ToArray) 
+        SQLHelper.ExecuteNonQuery(DataAccessManager.Connection, CommandType.Text, sb.ToString(), paramList.ToArray)
 
-    Return True 
+        Return True
 
-End Function
+    End Function
+
+    ''' <summary>
+    ''' 
+    ''' 情報を削除する
+    ''' </summary>
+    '''<param name="edpNo_key">edp_no</param>
+    ''' <returns>情報</returns>
+    ''' <remarks></remarks>
+    ''' <history>
+    ''' <para>2018/10/30  李松涛さん 新規作成 </para>
+    ''' </history>
+
+    Public Function DelMEdp(ByVal edpNo_key As String) As Boolean
+        'EMAB障害対応情報の格納処理
+        EMAB.AddMethodEntrance(MyClass.GetType.FullName & "." & MyMethod.GetCurrentMethod.Name, _
+               edpNo_key)
+        'SQLコメント
+        '--**テーブル： : m_edp
+        Dim sb As New StringBuilder
+        'SQL文
+        sb.AppendLine("DELETE FROM m_edp")
+        sb.AppendLine("WHERE 1=1")
+        If edpNo_key <> "" Then
+            sb.AppendLine("AND edp_no=@edp_no_key")   'edp_no
+        End If
+
+        'バラメタ格納
+        Dim paramList As New List(Of SqlParameter)
+        paramList.Add(MakeParam("@edp_no_key", SqlDbType.VarChar, 20, edpNo_key))
+
+
+        SQLHelper.ExecuteNonQuery(DataAccessManager.Connection, CommandType.Text, sb.ToString(), paramList.ToArray)
+
+        Return True
+
+    End Function
 
 End Class
